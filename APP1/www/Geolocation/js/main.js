9a2855fc3dc15a63e30a0499f65a1fc34ac0fd85
Cameron Mozie <cam25@fullsail.edu>
2012-12-04 16:04:31 -0500

    adding query to geo function

--- APP1/www/Geolocation/js/main.js
+++ APP1/www/Geolocation/js/main.js
@@ -17,15 +17,13 @@
     // onSuccess Geolocation
     //
  var gpsYes = function(location) {
-    var display = document.getElementById("geo");
-    var html = [];
+    var display = $("#geo"); // grabbing the div id geo
+    var html = []; //array im pushing the data inside
     var latitude = location.coords.latitude;
     var longitude = location.coords.longitude;
     var accuracy = location.coords.accuracy;
-    html.push("<img class='map' width='256' height='256' src='http://maps.google.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&markers=size:small|color:red|" + latitude + "," + longitude + "&zoom=14&size=256x256&sensor=false' />");
-    html.push("<p>Longitude: " + longitude + "</p>");
-    html.push("<p>Latitude: " + latitude + "</p>");
-    html.push("<p>Accuracy: " + accuracy + " meters</p>");
+    $("#geo").append("<img class='map' width='256' height='256' src='http://maps.google.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&markers=size:small|color:red|" + latitude + "," + longitude + "&zoom=14&size=256x256&sensor=false' />" +  "<p>Longitude: " + longitude + "</p>" + "<p>Latitude: " + latitude + "</p>" + "<p>Accuracy: " + accuracy + " meters</p>" );//pushing the map inside the arr)
+
     display.innerHTML = html.join("");
 }
 
@@ -34,3 +32,4 @@
    var gpsNo = function(error) {
     alert('Gps Fail ' + error.message);
 } 
+onDeviceReady()
