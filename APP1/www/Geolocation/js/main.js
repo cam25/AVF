c80ace1edbfe295a0802029b4fe7e41ffa2601ad
Cameron Mozie <cam25@fullsail.edu>
2012-12-04 12:47:19 -0500

    Updates

--- APP1/www/Geolocation/js/main.js
+++ APP1/www/Geolocation/js/main.js
@@ -10,28 +10,24 @@
     function onDeviceReady() {
         // Throw an error if no update is received every 30 seconds
         var options = { timeout: 10000 };
-        watchID = navigator.geolocation.watchPosition(onSuccess, onError, options);
+        watchID = navigator.geolocation.watchPosition(gpsYes, gpsNo, options);
     }
     
 
     // onSuccess Geolocation
     //
-   function onSuccess(position) {
-        var element = document.getElementById('coordinates');
-        element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
-                            'Longitude: '          + position.coords.longitude             + '<br />' +
-                            'Altitude: '           + position.coords.altitude              + '<br />' +
-                            'Accuracy: '           + position.coords.accuracy              + '<br />' +
-                            'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
-                            'Heading: '            + position.coords.heading               + '<br />' +
-                            'Speed: '              + position.coords.speed                 + '<br />' +
-                            'Timestamp: '          + new Date(position.timestamp)          + '<br />';
-    }
+ var gpsYes = function(location) {
+    var message = document.getElementById("message"), html = [];
+    html.push("<img width='256' height='256' src='http://maps.google.com/maps/api/staticmap?center=", location.coords.latitude, ",", location.coords.longitude, "&markers=size:small|color:blue|", location.coords.latitude, ",", location.coords.longitude, "&zoom=14&size=256x256&sensor=false' />");
+    html.push("<p>Longitude: ", location.coords.longitude, "</p>");
+    html.push("<p>Latitude: ", location.coords.latitude, "</p>");
+    html.push("<p>Accuracy: ", location.coords.accuracy, " meters</p>");
+    message.innerHTML = html.join("");
+}
+
     // onError Callback receives a PositionError object
     //
-    function onError(error) {
-        alert('code: ' + error.code + '\n' +
-          'message: ' + error.message + '\n');
-    }
-    
+   var gpsNo = function(error) {
+    alert('Attempt to get location failed: ' + error.message);
+} 
 onDeviceReady();
