45f49c5e27cc109996abce3eb77f784c33e199e2
Cameron Mozie <cam25@fullsail.edu>
2012-12-06 17:26:37 -0500

    Trying an to solve error

--- APP1/www/Geolocation/js/main.js
+++ APP1/www/Geolocation/js/main.js
@@ -7,7 +7,7 @@
 
     // Cordova is ready
     //
-   function onDeviceReady() {
+   var onDeviceReady = function() {
         // Throw an error if no update is received every 30 seconds
         var options = { timeout: 10000 };
         navigator.geolocation.watchPosition(gpsYes, gpsNo, options);
@@ -36,3 +36,8 @@
     
     
 
+    $("#geoloc").on("click", function() {
+	    
+	    onDeviceReady();
+	    
+    };
